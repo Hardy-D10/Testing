8353c1e6427c6e08a5e257ffd951bead8281d829
Mr. Hardy <andrew.hardy@fwusd8.org>
2026-07-09 16:27:31 +0000

    From VS Code to Github

--- test.c
+++ test.c
@@ -4,6 +4,6 @@ int main(void){
   printf("Hello, what is your name? ");
   char name[50];
   scanf("%s", name);
-  printf("Hello %s", name);
+  printf("Hello %s\n", name);
   return 0;
 }
--- test.py
+++ test.py
@@ -1,1 +1,4 @@
+print("Hello")
+name = input("What is your name? ")
+print("Hello", name)
 
